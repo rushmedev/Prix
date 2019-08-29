a765f169d8dc9b9fdc88c9f13e534acff5c5d7f2
rushmedev <rushmedev@gmail.com>
2019-08-29 18:45:49 +0530

    Changed to HomePage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-//import logo from './logo.svg';
 import './App.css';
-//import HomePage from './pages/homepage/homepage.components';
-import Directory from './components/directory/directory.component';
+import HomePage from './pages/homepage/homepage.components';
 
 function App() {
   return (
     <div>
-      <Directory/>
+      <HomePage/>
     </div>
   );
 }
